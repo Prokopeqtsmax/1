f650e54de081d04168a265a2cd13550383557822
Prokopets Max <lopxch@gmail.com>
2024-03-08 18:24:12 +0200

    1

---
+++ main.py
@@ -0,0 +1,19 @@
+class student:
+    name="Alex"
+
+
+
+    student1=Student()
+    print(student1.name)
+
+    class Student:
+        def __init__(self):
+            self.height = 170
+
+        height = 160
+
+        def printer(self):
+            print(self.height)
+
+    nick = Student()
+    nick.printer()
